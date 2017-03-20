35b213fe8a1ddffb9b7bbffefd5086914777652b
20151104705 <lwbJY666>
2017-03-20 11:30:55 +0800

    最终的计算器

--- jisuanqi/ViewController.swift
+++ jisuanqi/ViewController.swift
@@ -10,16 +10,60 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+   
+    @IBOutlet weak var display: UILabel!
+    var userlslnTheMiddleOfTypingANumber:Bool=false
+
+    @IBAction func operate(_ sender: UIButton) {
+        let operation = sender.currentTitle!
+        if userlslnTheMiddleOfTypingANumber{
+            enter()
+        }
+        switch operation {
+        case "×":perfomOperation(operation: {(op1:Double,op2:Double)->Double in
+            return op1 * op2
+        })
+        case "÷":perfomOperation(operation: {(op1:Double,op2:Double)->Double in
+            return op2 / op1})
+        case "+":perfomOperation(operation: {(op1:Double,op2:Double)->Double in
+            return op1 + op2})
+        case "−":perfomOperation(operation: {(op1:Double,op2:Double)->Double in
+            return op1 - op2})
+        default:
+            break
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func perfomOperation(operation:(Double,Double)->Double){
+        if operandStack.count>=2 {
+            displayValue = operation(operandStack.removeLast(),operandStack.removeLast())
+            enter()
     }
-
-
+    }
+    @IBAction func appendDight(_ sender: UIButton) {
+        let dight = sender.currentTitle!
+        if userlslnTheMiddleOfTypingANumber{
+            display.text=display.text!+dight
+        }else{
+            display.text=dight
+            userlslnTheMiddleOfTypingANumber=true
+        }
+    }
+    var operandStack = Array<Double>()
+    @IBAction func enter() {
+        userlslnTheMiddleOfTypingANumber=false
+        operandStack.append(displayValue)
+        print("operandStack=\(operandStack)")
+     
+    }
+    var displayValue:Double{
+        get {
+            return NumberFormatter().number(from: display.text!)!.doubleValue
+    }
+        set {
+            display.text="\(newValue)"
+            userlslnTheMiddleOfTypingANumber = false
+        }
+        
 }
 
+}
